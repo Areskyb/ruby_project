f66d7bcbee8e9e90abdb0bca299b9086fecf2393
Aresky Berkane <areskyberkane@gmail.com>
2019-11-09 15:42:30 +0000

    cleared some console bugs

--- app/db/seeds.rb
+++ app/db/seeds.rb
@@ -10,7 +10,7 @@ City.delete_all
 
 spain = Country.new('name' => 'Spain', 'visited' => true)
 uk = Country.new('name' => 'uk', 'visited' => true)
-france = Country.new('name' => 'france', 'visited' => true)
+france = Country.new('name' => 'france', 'visited' => false)
 andorra = Country.new('name' => 'andorra', 'visited' => true)
 
 andorra.save
@@ -21,7 +21,7 @@ spain.save
 seville = City.new('name' => 'Seville','visited' => true,'country_id' => spain.id)
 palma = City.new('name' => 'palma','visited' => false,'country_id' => spain.id)
 paris = City.new('name' => 'paris','visited' => true,'country_id' => france.id)
-les_termes = City.new('name' => 'les_termes','visited' => true,'country_id' => france.id)
+les_termes = City.new('name' => 'les_termes','visited' => false,'country_id' => france.id)
 cambridge = City.new('name' => 'cambridge','visited' => false,'country_id' => uk.id)
 edimburgh = City.new('name' => 'edimburgh','visited' => true,'country_id' => uk.id)
 la_bella = City.new('name' => 'la bella','visited' => false,'country_id' => andorra.id)
